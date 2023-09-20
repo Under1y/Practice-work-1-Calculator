d84973dd464d0ba8e2f11de1c49d1690d2c06287
Under1y <142081094+Under1y@users.noreply.github.com>
2023-09-20 14:29:09 +0300

    Я сделал кальтулятор

---
+++ Практическая работа №1 Калькулятор/Program.cs
@@ -0,0 +1,24 @@
+﻿while (true)
+{
+    float x = float.Parse(Console.ReadLine());
+    string z = Console.ReadLine();
+    switch (z)
+    {
+        case "+":
+            float y = float.Parse(Console.ReadLine());
+            Console.WriteLine(x + y);
+            break;
+        case "-":
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine(x - y);
+            break;
+        case "*":
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine(x * y);
+            break;
+        case "/":
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine(x / y);
+            break;
+    }
+}
