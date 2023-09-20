c1c7a715c170a9990cdbf68c74c790bbd4d08e8a
Under1y <142081094+Under1y@users.noreply.github.com>
2023-09-20 21:38:47 +0300

    Обновление

--- Практическая работа №1 Калькулятор/Program.cs
+++ Практическая работа №1 Калькулятор/Program.cs
@@ -1,7 +1,10 @@
-﻿while (true)
+﻿Console.WriteLine("сложение +\nвычитание -\nумножение *\nделение /\nвозведение в степень **\nквадратный корень sqrt\nпроцент от числа %\nостаток //");
+bool w = true;
+while (w)
 {
     float x = float.Parse(Console.ReadLine());
     string z = Console.ReadLine();
+    double n;
     switch (z)
     {
         case "+":
@@ -19,6 +22,24 @@
         case "/":
             y = float.Parse(Console.ReadLine());
             Console.WriteLine(x / y);
+            break;
+        case "//":
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine(x % y);
+            break;
+        case "%":
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine((x / 100) * y);
+            break;
+        case "**":
+            n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Pow(x, n));
+            break;
+        case "sqrt":
+            Console.WriteLine(Math.Sqrt(x));
+            break;
+        case "x":
+            w = false;
             break;
     }
 }
